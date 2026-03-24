e08ed7dd9986bd65d681c1814f988fafcab19f7f
Cristian Simion <204784908+cristian-smn@users.noreply.github.com>
2026-03-24 22:18:16 +0200

    24-mar-2026 commit

---
+++ Topics/Comments/Javadoc comment/src/Main.java
@@ -0,0 +1,8 @@
+/**
+     * It prints the text "It works correctly" to the standard output
+*/
+class Main {
+    public static void main(String[] args) {
+        System.out.println("It works correctly");
+    }
+}
